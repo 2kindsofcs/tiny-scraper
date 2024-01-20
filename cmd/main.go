8d1632bdf5aeb507f67deb995214fce10a96899a
2kindsofcs <2kindsofcs@gmail.com>
2024-01-20 19:09:27 +0900

    특정 키워드에 대한 팟캐스트 이름 및 링크 획득

--- cmd/main.go
+++ cmd/main.go
@@ -18,43 +18,44 @@ func main() {
 	// 5초에 한 번씩 웹페이지들을 긁어서 정보를 저장
 	//
 	for _, endpoint := range endpoints {
-		content, err := scrapeKubepodcast(endpoint)
+		title, link, err := scrapeKubepodcast(endpoint)
 		if err != nil {
 			//do something
 		}
-		fmt.Println(content)
+		fmt.Println(title, link)
 		fmt.Println("end")
 		continue
 	}
 }
 
-func scrapeKubepodcast(endpoint string) (content string, err error) {
+func scrapeKubepodcast(endpoint string) (title, link string, err error) {
 	resp, err := http.Get(endpoint)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return "", err
+		return "", "", err
 	}
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	doc.Find("div.episode h3").Each(func(i int, s *goquery.Selection) {
 		if strings.Contains(strings.ToLower(s.Text()), "istio") {
-			link, ok := s.Find("a").First().Attr("href")
+			l, ok := s.Find("a").First().Attr("href")
 			if !ok {
 				panic("link should exist.")
 			}
-			content = link
+			title = s.Text()
+			link = l
 		}
 	})
 
-	return content, nil
+	return title, link, nil
 }
 
 // utf-8인지 검사하는 유틸 함수. 꼭 여기 있을 필요는 없음.
